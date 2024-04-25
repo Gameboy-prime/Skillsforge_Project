85c3c90c2ca2e7b4c546dfea5d9f08e524ba1bbb
Craigshakesphere-Makinde <117420707+Craigshakesphere-Makinde@users.noreply.github.com>
2024-04-25 16:12:36 +0100

    Create WallSpawner.cs
    This is the script need to continously spawn the level section for the game

---
+++ Skillsforge Project/Assets/Scripts/WallSpawner.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class WallSpawner : MonoBehaviour
+{
+    public Transform spawnPos;
+    public ObjectPool<GameObject> pool;
+    public GameObject block;
+    public float rad=3f;
+
+    public float timeSec;
+    private float timeMilli;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        pool = new ObjectPool<GameObject>(() =>
+        {
+            return Instantiate(block);
+
+
+        },
+        obj =>
+        {
+            obj.gameObject.SetActive(true);
+            obj.gameObject.GetComponentInChildren<Multiplier>().CallDeterminatState();
+        },
+        obj =>
+        {
+            obj.gameObject.SetActive(false);
+        },
+        obj =>
+        {
+            Destroy(obj.gameObject);
+        }, false, 10, 15);
+            
+
+        
+    }
+
+
+    public void Spawn()
+    {
+        //SpawnTime();
+        GameObject spawnee = pool.Get();
+        spawnee.transform.position = spawnPos.position;
+        spawnee.transform.rotation = Quaternion.identity;
+
+        //spawnPos.position += new Vector3(0, 0, 50);
+
+    }
+
+    public void DespawnBlock(GameObject blockObject)
+    {
+        pool.Release(blockObject);
+
+    }
+
+    
+
+    private void Start()
+    {
+        //InvokeRepeating(nameof(Spawn), rad, rad);
+    }
+
+    private void Update()
+    {
+        timeMilli+= Time.deltaTime*10;
+        if(timeMilli>10)
+        {
+            timeMilli = 0;
+            timeSec += 1;
+            if (timeSec > rad)
+            {
+                Spawn();
+                timeSec = 0;
+            }
+        }
+        
+    }
+
+}
