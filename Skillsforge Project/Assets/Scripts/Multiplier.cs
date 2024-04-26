392953f80e0cbe0a6e87c40988786dc1a15b6170
Craigshakesphere-Makinde <117420707+Craigshakesphere-Makinde@users.noreply.github.com>
2024-04-26 18:56:45 +0100

    Create Multiplier.cs
    This is the script thta is to determine the value of the multiplier

---
+++ Skillsforge Project/Assets/Scripts/Multiplier.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Multiplier : MonoBehaviour
+{
+    public TextMeshPro textBox;
+    public string multiplierState;
+    private string[] operators= { "÷", "-","+", "*" };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CallDeterminatState();
+        
+        
+        
+    }
+
+    public void CallDeterminatState()
+    {
+       // Debug.Log("CallDetermineState Function was called");
+        string op = operators[Random.Range(0, 4)];
+        string value = Random.Range(1, 5).ToString();
+        multiplierState = op + " " + value;
+        textBox.text = multiplierState;
+
+    }
+
+    
+}
