83ffdafd03c3d143b748827ab2f6cbe52c8c2b23
Craigshakesphere-Makinde <117420707+Craigshakesphere-Makinde@users.noreply.github.com>
2024-04-28 16:26:26 +0100

    Update ReleaseBlock.cs

--- Skillsforge Project/Assets/Scripts/ReleaseBlock.cs
+++ Skillsforge Project/Assets/Scripts/ReleaseBlock.cs
@@ -8,8 +8,15 @@ public class ReleaseBlock : MonoBehaviour
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
+        StartCoroutine(Release(other));
+        
+    }
+
+    IEnumerator Release(Collider other)
+    {
         if (other.CompareTag("Block"))
         {
+            yield return new WaitForSeconds(.2f);
             spawner.DespawnBlock(other.gameObject);
 
         }
