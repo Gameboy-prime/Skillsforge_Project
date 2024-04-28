9316f7aa58630072353ef621a6d5a86da8939b65
Craigshakesphere-Makinde <117420707+Craigshakesphere-Makinde@users.noreply.github.com>
2024-04-28 16:27:05 +0100

    Loading Bar and Movement for the enemy

---
+++ Skillsforge Project/Assets/Scripts/LoadingBar.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class LoadingBar : MonoBehaviour
+{
+    public Slider slider;
+    public int LevelIndex;
+    public TextMeshProUGUI loaderText;
+    public GameObject loaderBox;
+    // Start is called before the first frame update
+    
+    public void LoadLevel()
+    {
+        loaderBox.SetActive(true);
+        StartCoroutine(Loader(LevelIndex));
+    }
+
+    IEnumerator Loader(int index)
+    {
+        AsyncOperation operation= SceneManager.LoadSceneAsync(index);
+
+        while(!operation.isDone)
+        {
+            float value = operation.progress;
+            slider.value= value / 0.9f;
+            loaderText.text= Mathf.Round((value/0.9f)*100).ToString();
+            yield return null;
+
+
+
+        }
+
+
+    }
+}
