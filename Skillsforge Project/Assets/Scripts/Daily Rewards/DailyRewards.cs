383807c871f084e4f0f6296da4a7b01ba457fbf5
Craigshakesphere-Makinde <117420707+Craigshakesphere-Makinde@users.noreply.github.com>
2024-05-17 16:25:47 +0100

    Create DailyRewards.cs

---
+++ Skillsforge Project/Assets/Scripts/Daily Rewards/DailyRewards.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System;
+
+namespace DailyRewardSystem
+{
+
+    public enum RewardType {
+        FLASH, 
+        COINS
+    }
+
+    [Serializable] public struct Reward
+    {
+        public RewardType Type;
+        public int Amount;
+    }
+
+
+    public class DailyRewards : MonoBehaviour
+    {
+        [Header("Main Menu")]
+        [SerializeField] private TextMeshProUGUI coin;
+
+        [Space]
+
+        [Header("Reward UI")]
+        [SerializeField] private GameObject rewardBox;
+        [SerializeField] private Button openRewardButton;
+        [SerializeField] private Button closeRewardButton;
+
+       
+       
+         public GameObject rewardNotification;
+       
+
+        [Space]
+
+        [Header("Reward Sprit")]
+        [SerializeField] Sprite coinSprite;
+        [SerializeField] Sprite flashSprite;
+
+
+        
+
+        [Space]
+        [Header("Timing")]
+        [SerializeField] double rewardDelaySec = 20;
+        [SerializeField] float checkForRewardDelay = 5;
+
+        [Space]
+        [Header("Effects")]
+        [SerializeField] ParticleSystem coinParticle;
+       
+       
+        private void Start()
+        {
+            Initialize();
+            
+        }
+
+        void Initialize()
+        {
+            
+            UpdateCoin();
+
+            openRewardButton.onClick.RemoveAllListeners();
+            openRewardButton.onClick.AddListener(OpenRewardInit);
+
+            closeRewardButton.onClick.RemoveAllListeners();
+            closeRewardButton.onClick.AddListener(CloseRewardInit);
+
+            
+
+            
+
+        }
+
+        
+
+       
+
+        void OpenRewardInit()
+        {
+            rewardBox.SetActive(true);
+
+        }
+
+        void CloseRewardInit()
+        {
+            rewardBox.SetActive(false);
+
+        }
+
+        void UpdateCoin()
+        {
+            coin.text= GameData.Coins.ToString();
+
+        }
+
+
+        public void ClaimReward(int rewardsCoin)
+        {
+            GameData.coins += rewardsCoin;
+        }
+
+    }
+
+}
