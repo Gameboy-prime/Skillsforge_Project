3fc1e5d4cdaa0f0f926af5f376143b85fa9e9113
Craigshakesphere-Makinde <117420707+Craigshakesphere-Makinde@users.noreply.github.com>
2024-05-21 16:18:57 +0100

    Create PowerUp.cs

---
+++ Skillsforge Project/Assets/Scripts/Power Up/PowerUp.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public GameObject[] gunObjects;
+    public enum PowerUpType { 
+        Gun1, Gun2, Gun3
+    }
+
+    public PowerUpType type;
+
+    public void Activate()
+    {
+        int rad= Random.Range(0, gunObjects.Length);
+        if (rad == 0)
+        {
+            type = PowerUpType.Gun1;
+
+        }
+        else if (rad == 1)
+        {
+            type = PowerUpType.Gun2;
+        }
+
+        else if (rad == 2)
+        {
+            type = PowerUpType.Gun3;
+        }
+
+        ChooseGun(rad);
+    }
+
+    void ChooseGun(int gunNo)
+    {
+        for (int i =0; i< gunObjects.Length; i++)
+        {
+            gunObjects[i].gameObject.SetActive(false);
+        }
+
+        gunObjects[gunNo].gameObject.SetActive(true);
+
+        Debug.Log("The power up is " + gunNo);
+    }
+
+    
+
+}
