372d122c38875605217ae092ebda4efdd432ef04
Craigshakesphere-Makinde <117420707+Craigshakesphere-Makinde@users.noreply.github.com>
2024-05-25 10:41:22 +0100

    Update GameEnd.cs

--- Skillsforge Project/Assets/Scripts/Environment/GameEnd.cs
+++ Skillsforge Project/Assets/Scripts/Environment/GameEnd.cs
@@ -4,47 +4,28 @@ using UnityEngine;
 
 public class GameEnd : MonoBehaviour
 {
-    [SerializeField] public GameObject gameOverBox;
-    public EndGame endGame;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            Debug.Log("Player has entered Trigger");
-            endGame.GetComponent<EndGame>().enabled = false;
-            gameOverBox.SetActive(true);
-            float num = Difficulty.difficulty;
-            float check = PlayerPrefs.GetFloat("difficulty");
-
-            if(num>=check)
-            {
-                PlayerPrefs.SetFloat("difficulty", num+1);
-            }
-            
-            
-           
-            PlayerPrefs.Save();
-            Invoke(nameof(HeyEnd),.3f);
-
-
-        }
-    }
+    
 
     public void HeyEnd()
     {
         Time.timeScale = 0;
 
+    }
+
+    public void SaveProgress()
+    {
+       
+        float num = Difficulty.difficulty;
+        float check = PlayerPrefs.GetFloat("difficulty");
+
+        if (num >= check)
+        {
+            PlayerPrefs.SetFloat("difficulty", num + 1);
+        }
+
+
+
+        PlayerPrefs.Save();
+        
     }
 }
