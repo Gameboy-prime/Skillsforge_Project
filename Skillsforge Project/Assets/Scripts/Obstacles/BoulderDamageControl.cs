d1a3335ac17b8b16480e92103e4225f17762ede5
Craigshakesphere-Makinde <117420707+Craigshakesphere-Makinde@users.noreply.github.com>
2024-05-25 10:41:47 +0100

    Update BoulderDamageControl.cs

--- Skillsforge Project/Assets/Scripts/Obstacles/BoulderDamageControl.cs
+++ Skillsforge Project/Assets/Scripts/Obstacles/BoulderDamageControl.cs
@@ -12,11 +12,16 @@ public class BoulderDamageControl : MonoBehaviour
     public GameObject sphere;
 
     [SerializeField] ParticleSystem effect;
+
+    private PowerUp powerUp;
+
+
    
     private void Start()
     {
         
         currentHealth = health;
+        powerUp= GetComponent<PowerUp>();
         
     }
     public void TakeDamage(int damage)
@@ -27,6 +32,24 @@ public class BoulderDamageControl : MonoBehaviour
 
         if (currentHealth <= 0)
         {
+            if (powerUp.type == PowerUp.PowerUpType.Gun1)
+            {
+                WeaponHandler.gunNo = 1;
+            }
+            else if (powerUp.type == PowerUp.PowerUpType.Gun2)
+            {
+                WeaponHandler.gunNo = 2;
+            }
+            else if(powerUp.type == PowerUp.PowerUpType.Gun3)
+            {
+                WeaponHandler.gunNo = 3;
+            }
+
+            else if(powerUp.type == PowerUp.PowerUpType.Gun4)
+            {
+                WeaponHandler.gunNo = 4;
+            }
+
             Destroy();
         }
 
@@ -55,8 +78,8 @@ public class BoulderDamageControl : MonoBehaviour
         
         effect.Play();
         sphere.SetActive(false);
-        yield return new WaitForSeconds(1.4f);
-        effect.Stop();
+        yield return new WaitForSeconds(1f);
+        //effect.Stop();
         
 
         FindObjectOfType<PowerupSpawner>().pool.Release(this.gameObject);
