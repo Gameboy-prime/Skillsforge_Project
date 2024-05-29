086d7e35a1fbe67b3830f09fb93ea60f06aef424
Craigshakesphere-Makinde <117420707+Craigshakesphere-Makinde@users.noreply.github.com>
2024-05-29 12:09:11 +0100

    Update Bullet.cs

--- Skillsforge Project/Assets/Scripts/Weapons/Bullet.cs
+++ Skillsforge Project/Assets/Scripts/Weapons/Bullet.cs
@@ -10,75 +10,99 @@ public class Bullet : MonoBehaviour
     {
         TypeA, TypeB, TypeC, TypeD
     }
-    [HideInInspector] public BulletType bulletType;
+    public BulletType bulletType;
     
     private void OnTriggerEnter(Collider other)
-    {
+    {//
+        //Debug.Log("The Bullet has entered a trigger");
         if (other.CompareTag("enemy"))
         {
+            //Debug.Log("It Has collided with enemy");
             DamageControl damageControl= other.GetComponent<DamageControl>();
             damageControl.TakeDamage(damage);
 
 
-            if (bulletType == BulletType.TypeA)
-            {
-                FindObjectOfType<WeaponHandler>().bulletPool1.Release(gameObject);
-            }
-            else if(bulletType== BulletType.TypeB)
-            {
-                FindObjectOfType<WeaponHandler>().bulletPool2.Release(gameObject);
-            }
-            else if (bulletType == BulletType.TypeC)
-            {
-                FindObjectOfType<WeaponHandler>().bulletPool3.Release(gameObject);
-            }
-            else if (bulletType == BulletType.TypeD)
-            {
-                FindObjectOfType<WeaponHandler>().bulletPool4.Release(gameObject);
-            }
+                if (bulletType == BulletType.TypeA)
+                {
+                    WeaponHandler.bulletPools[0].Release(this.gameObject);
+                }
+                else if(bulletType== BulletType.TypeB)
+                {
+                    WeaponHandler.bulletPools[1].Release(this.gameObject);
+                }
+                else if (bulletType == BulletType.TypeC)
+                {
+                    WeaponHandler.bulletPools[2].Release(this.gameObject);
+                }
+                else if (bulletType == BulletType.TypeD)
+                {
+                    WeaponHandler.bulletPools[3].Release(this.gameObject);
+                }
 
 
         }
 
-        if(other.CompareTag("Boundary"))
+        else if (other.CompareTag("Boundary"))
         {
+
             if (bulletType == BulletType.TypeA)
             {
-                FindObjectOfType<WeaponHandler>().bulletPool1.Release(gameObject);
+                WeaponHandler.bulletPools[0].Release(this.gameObject);
             }
             else if (bulletType == BulletType.TypeB)
             {
-                FindObjectOfType<WeaponHandler>().bulletPool2.Release(gameObject);
+                WeaponHandler.bulletPools[1].Release(this.gameObject);
             }
             else if (bulletType == BulletType.TypeC)
             {
-                FindObjectOfType<WeaponHandler>().bulletPool3.Release(gameObject);
+                WeaponHandler.bulletPools[2].Release(this.gameObject);
             }
             else if (bulletType == BulletType.TypeD)
             {
-                FindObjectOfType<WeaponHandler>().bulletPool4.Release(gameObject);
+                WeaponHandler.bulletPools[3].Release(this.gameObject);
             }
+
+            /* if (bulletType == BulletType.TypeA)
+             {
+                 Debug.Log("THe bullet type that was released is the type A");
+                 WeaponHandler.Instance.GetBulletPool(0).Release(gameObject);
+             }
+             else if (bulletType == BulletType.TypeB)
+             {
+                 WeaponHandler.Instance.GetBulletPool(1).Release(gameObject);
+             }
+             else if (bulletType == BulletType.TypeC)
+             {
+                 WeaponHandler.Instance.GetBulletPool(2).Release(gameObject);
+             }
+             else if (bulletType == BulletType.TypeD)
+             {
+                 WeaponHandler.Instance.GetBulletPool(3).Release(gameObject);
+             }*/
         }
 
         if (other.CompareTag("Boulder"))
         {
+            //Debug.Log("It has collided with Boulder");
             BoulderDamageControl damageControl= other.GetComponent<BoulderDamageControl>();
             damageControl.TakeDamage(damage);
+
+
             if (bulletType == BulletType.TypeA)
             {
-                FindObjectOfType<WeaponHandler>().bulletPool1.Release(gameObject);
+                WeaponHandler.bulletPools[0].Release(this.gameObject);
             }
             else if (bulletType == BulletType.TypeB)
             {
-                FindObjectOfType<WeaponHandler>().bulletPool2.Release(gameObject);
+                WeaponHandler.bulletPools[1].Release(this.gameObject);
             }
             else if (bulletType == BulletType.TypeC)
             {
-                FindObjectOfType<WeaponHandler>().bulletPool3.Release(gameObject);
+                WeaponHandler.bulletPools[2].Release(this.gameObject);
             }
             else if (bulletType == BulletType.TypeD)
             {
-                FindObjectOfType<WeaponHandler>().bulletPool4.Release(gameObject);
+                WeaponHandler.bulletPools[3].Release(this.gameObject);
             }
         }
     }
